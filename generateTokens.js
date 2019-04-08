8c44f30d47cf0e9a37e8475be701a0c2010571d4
Tpt-John Hatcher <john.hatcher@teacherspayteachers.com>
2019-04-08 13:02:57 -0400

    adding linting!

--- generateTokens.js
+++ generateTokens.js
@@ -1,8 +1,7 @@
 const GoogleSheet = require('./lib/GoogleSheet');
-const config = require('./package.json').config;
+const { config } = require('./package.json');
 
 const google = new GoogleSheet(config.sheets);
 
-//google.generateAuthUrl(['https://www.googleapis.com/auth/spreadsheets']);
-
-google.generateToken("4/JgEeSkzEYARfJltjiu-aObT9hoTRKkKprnc7TTUre2SwE9knrYo5eII");
+google.generateAuthUrl(['https://www.googleapis.com/auth/spreadsheets']);
+google.generateToken('4/JgEeSkzEYARfJltjiu-aObT9hoTRKkKprnc7TTUre2SwE9knrYo5eII');
--- index.js
+++ index.js
@@ -2,50 +2,50 @@ const moment = require('moment');
 const GoogleSheet = require('./lib/GoogleSheet');
 const GoogleCalendar = require('./lib/GoogleCalendar');
 const GoogleMail = require('./lib/GoogleMail');
-const config = require('./package.json').config;
+const { config } = require('./package.json');
 
 const sheet = new GoogleSheet(config.sheets);
 const calendar = new GoogleCalendar(config.calendar);
 const gmail = new GoogleMail(config.gmail);
 
 const nowTimestamp = moment().format('YYYY-MM-DD');
-const from = moment().subtract(12, 'months'); 
-const to = moment().add(12, 'months'); 
+const from = moment().subtract(12, 'months');
+const to = moment().add(12, 'months');
 
-exports.handler = async function(event, context, callback) {   
-	try {
-		// TODO: do them in parallel
-		await sheet.load(config.sheets.id, config.sheets.ranges.data);
-		await calendar.load(from, to);
+exports.handler = async () => {
+  try {
+    // TODO: do them in parallel
+    await sheet.load(config.sheets.id, config.sheets.ranges.data);
+    await calendar.load(from, to);
 
-		const scoreData = sheet.rows.map((person, index) => {
-			person.score = calendar.computeFrequencyScore(person.email, person.targetFrequency);
-			person.lastSeen = calendar.findLastEventDate(person.email);
+    const scoreData = sheet.rows.map((person) => {
+      /* eslint no-param-reassign:0 */
+      person.score = calendar.computeFrequencyScore(person.email, person.targetFrequency);
+      person.lastSeen = calendar.findLastEventDate(person.email);
 
-			return [person.score, person.lastSeen, nowTimestamp];
-		});
+      return [person.score, person.lastSeen, nowTimestamp];
+    });
 
-		await sheet.update(config.sheets.id, config.sheets.ranges.writes, scoreData);
+    await sheet.update(config.sheets.id, config.sheets.ranges.writes, scoreData);
 
-		const winners = sheet.rows
-		  .filter(person => !calendar.isScheduled(person.email))
-			.filter(person => person.score === 0)
-			.splice(0, 10);
+    const winners = sheet.rows
+      .filter(person => !calendar.isScheduled(person.email))
+      .filter(person => person.score === 0)
+      .splice(0, 10);
 
-		let html = '<table>';
+    let html = '<table>';
 
-		html += '<th><td>Name</td><td>Score</td><td>Last Seen</td></th>'
-		winners.forEach(person => {
-			html += `<tr><td>${person.first} ${person.last}</td><td>${person.score}</td><td>${person.lastSeen}</td></tr>`;
-		})
+    html += '<th><td>Name</td><td>Score</td><td>Last Seen</td></th>';
+    winners.forEach((person) => {
+      html += `<tr><td>${person.first} ${person.last}</td><td>${person.score}</td><td>${person.lastSeen}</td></tr>`;
+    });
 
-		html += '</table>';
-		html += '<a href="https://docs.google.com/spreadsheets/d/11TtAui1fy4HXbDaqB_mlHVzv_hdJw5jD8w7c7h05oaY">Full Directory"</a>';
+    html += '</table>';
+    html += '<a href="https://docs.google.com/spreadsheets/d/11TtAui1fy4HXbDaqB_mlHVzv_hdJw5jD8w7c7h05oaY">Full Directory"</a>';
 
-		await gmail.send(`[Social Directory] ${new Date()}`, html);
-	}
-	catch(error) {
-	  await gmail.send(`[Social Directory] ${new Date()} ERROR!`, `<pre>${error.stack}</pre>`);
-		throw error;
-	}
+    await gmail.send(`[Social Directory] ${new Date()}`, html);
+  } catch (error) {
+    await gmail.send(`[Social Directory] ${new Date()} ERROR!`, `<pre>${error.stack}</pre>`);
+    throw error;
+  }
 };
